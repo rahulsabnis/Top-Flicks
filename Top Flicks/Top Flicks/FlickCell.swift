bdc7c57be1238caf317ce8b8a0fb28e721850ea9
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-01-12 13:47:55 -0800

    Finished Required User Stories for Top Flicks app.

---
+++ Top Flicks/Top Flicks/FlickCell.swift
@@ -0,0 +1,24 @@
+//
+//  FlickCell.swift
+//  Top Flicks
+//
+//  Created by Rahul Sabnis on 1/6/17.
+//  Copyright © 2017 Rahul Sabnis. All rights reserved.
+//
+
+import UIKit
+
+class FlickCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
